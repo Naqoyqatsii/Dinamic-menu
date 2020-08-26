071a34a8eeb519c69368d5d46e785e19236bc7b7
Koyaanisqatsi <827779@gmail.com>
2020-08-26 22:57:42 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsDZ29081
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TopLevelMenu = new System.Windows.Forms.TextBox();
+			this.SubItem = new System.Windows.Forms.TextBox();
+			this.AddPrimary = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// TopLevelMenu
+			// 
+			this.TopLevelMenu.Location = new System.Drawing.Point(66, 243);
+			this.TopLevelMenu.Name = "TopLevelMenu";
+			this.TopLevelMenu.Size = new System.Drawing.Size(331, 22);
+			this.TopLevelMenu.TabIndex = 0;
+			// 
+			// SubItem
+			// 
+			this.SubItem.Location = new System.Drawing.Point(66, 307);
+			this.SubItem.Name = "SubItem";
+			this.SubItem.Size = new System.Drawing.Size(330, 22);
+			this.SubItem.TabIndex = 1;
+			// 
+			// AddPrimary
+			// 
+			this.AddPrimary.Location = new System.Drawing.Point(427, 241);
+			this.AddPrimary.Name = "AddPrimary";
+			this.AddPrimary.Size = new System.Drawing.Size(235, 28);
+			this.AddPrimary.TabIndex = 2;
+			this.AddPrimary.Text = "Добавить пункт меню";
+			this.AddPrimary.UseVisualStyleBackColor = true;
+			this.AddPrimary.Click += new System.EventHandler(this.AddPrimary_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(427, 305);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(235, 28);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Добавить подменю";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.AddPrimary);
+			this.Controls.Add(this.SubItem);
+			this.Controls.Add(this.TopLevelMenu);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox TopLevelMenu;
+		private System.Windows.Forms.TextBox SubItem;
+		private System.Windows.Forms.Button AddPrimary;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDZ29081
+{
+	public partial class Form1 : Form
+	{
+		MenuStrip menuStrip;
+		ToolStripMenuItem toolStrip;
+		public Form1()
+		{
+			InitializeComponent();
+			menuStrip = new MenuStrip();
+			menuStrip.Visible = false;
+			Controls.Add(menuStrip);
+			
+		}
+
+		private void AddPrimary_Click(object sender, EventArgs e)
+		{
+			if (TopLevelMenu.Text.Length == 0)
+			{
+				//Тут можно бы вывести сообщение
+				//MessageBox.Show("Введите название пункта основного меню!");
+				return;
+			}
+			menuStrip.Visible = true;
+			toolStrip = new ToolStripMenuItem(TopLevelMenu.Text);
+			toolStrip.MouseDown += ToolStrip_MouseDown;
+			menuStrip.Items.Add(toolStrip);
+		}
+
+		private void ToolStrip_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Right)
+			{
+				menuStrip.Items.Remove(sender as ToolStripMenuItem);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if(menuStrip.Items.Count == 0)
+			{
+				//Тут можно бы вывести сообщение
+				//MessageBox.Show("Нет ни одного пункта меню!");
+				return;
+			}
+			if(SubItem.Text.Length == 0)
+			{
+				//Тут можно бы вывести сообщение
+				//MessageBox.Show("Введите название пункта подменю!");
+				return;
+			}
+			toolStrip.DropDownItems.Add(SubItem.Text);
+		}
+	}
+}
